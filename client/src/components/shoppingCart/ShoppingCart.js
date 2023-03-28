8bf2c13e184c0bab6d7f1c55441e855fc2e2eb3b
kvlin <kevlin129@hotmail.com>
2023-03-29 00:10:29 +1100

    Able to remove single item from the cart

--- client/src/components/shoppingCart/ShoppingCart.js
+++ client/src/components/shoppingCart/ShoppingCart.js
@@ -4,7 +4,7 @@ const ShoppingCart = () => {
 
     const user = useContext(AuthContext);
     console.log(user.user);
-    const [items, setItems] = useState([{ name: "coke", price: 2.00 }, { name: "sprite", price: 3.00 }]);
+    const [items, setItems] = useState([]);
     // use useEffect to fetch items from the database and set them to the items state with request to 'api/shoppingCart/:id'
     useEffect(() => {
         const fetchData = async () => {
@@ -15,31 +15,57 @@ const ShoppingCart = () => {
         }
         fetchData();
     }, [])
-    const addItem = (item) => {
-        setItems([...items, item]);
-    };
+    // const addItem = (item) => {
+    //     setItems([...items, item]);
+    // };
 
-    const removeItem = (index) => {
+    const removeItem = async (index) => {
         const newItems = [...items];
         newItems.splice(index, 1);
+        const res = await fetch(`/api/cart`, {
+            method: 'DELETE',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ toDelete: items[index].name })
+        });
+        const data = await res.json();
+
         setItems(newItems);
     };
+
+    const clearCart = async () => {
+        // fetch delete method api with '/api/cartAll' 
+        const res = await fetch(`/api/cartAll`, {
+            method: 'DELETE'
+        });
+        const data = await res.json();
+        if (data.cleared) {
+            setItems([]);
+        }
+
+    }
 
     const total = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
     return (
         <div>
             <h2>Shopping Cart</h2>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>
-                        {item.name} - ${item.price}
-                        <button onClick={() => removeItem(index)}>Remove</button>
-                    </li>
-                ))}
-            </ul>
-            <p>Total: ${total}</p>
-            <button onClick={() => setItems([])}>Clear Cart</button>
+            {items.length === 0 ?
+                <p>Your cart is empty</p> :
+                <ul>
+                    {items.map((item, index) => (
+                        <li key={index}>
+                            {item.name} - ${item.price}
+                            <button onClick={() => removeItem(index)}>Remove</button>
+                        </li>
+                    ))}
+                </ul>
+            }
+
+            {items.length !== 0 && <p>Total: ${total}</p>}
+            {items.length !== 0 && <button onClick={() => clearCart()}>Clear Cart</button>}
         </div>
     );
 };
--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -144,6 +144,100 @@ router.put('/cart', async (req, res) => {
     }
 })
 
+// Remove one item from the cart list
+router.delete('/cart', async (req, res) => {
+    // check if the user is not logged in
+    if (!req.isAuthenticated()) {
+        return
+    }
+    // query cart from the backend rather passing from the front end, for quicker processing
+    // check if item is already in the cart
+    try {
+        const params = {
+            TableName: table,
+            ExpressionAttributeNames: {
+                "#user": "user"
+            },
+            // values use aliases with : prefix 
+            ExpressionAttributeValues: {
+                ":email": req.user.email
+            },
+            // specifies the search criteria
+            KeyConditionExpression: "#user = :email",
+
+        };
+        // Scan return all items in the table
+        docClient.query(params, (err, results) => {
+            if (err) {
+                res.status(500).json(err)
+            } else {
+                let fetchedCart = results.Items[0].cart;
+                handleUpdate(fetchedCart)
+            }
+        })
+    } catch (err) {
+        console.log("Failure", err.message)
+    }
+    const handleUpdate = async (fetchedCart) => {
+        // remove the item object from the cart array
+        console.log(fetchedCart)
+        console.log(req.body, "to delete")
+
+        const extractedCart = fetchedCart.filter(item => item.name !== req.body.toDelete)
+        const updatedCart = [...extractedCart]
+        const params = {
+            TableName: table,
+            Key: { "user": req.user.email },
+            ReturnValues: 'ALL_NEW',
+            UpdateExpression: 'set #cart = :items',
+            ExpressionAttributeNames: {
+                '#cart': 'cart'
+            },
+            ExpressionAttributeValues: {
+                ':items': updatedCart,
+            }
+
+        };
+        try {
+            const data = await docClient.update(params).promise()
+            console.log("Success")
+            console.log(data)
+            res.json({ deleted: true, item: req.body.toDelete })
+        } catch (err) {
+            console.log("Failure", err.message)
+        }
+    }
+})
+
+
+// Empty the cart for the user
+router.delete('/cartAll', async (req, res) => {
+    // check if the user is not logged in
+    if (!req.isAuthenticated()) {
+        return
+    }
+    const params = {
+        TableName: table,
+        Key: { "user": req.user.email },
+        ReturnValues: 'ALL_NEW',
+        UpdateExpression: 'set #cart = :empty_list',
+        ExpressionAttributeNames: {
+            '#cart': 'cart'
+        },
+        ExpressionAttributeValues: {
+            ':empty_list': []
+        }
+
+    };
+    try {
+        const data = await docClient.update(params).promise()
+        console.log("Success")
+        console.log(data)
+        res.json({ cleared: true })
+    } catch (err) {
+        console.log("Failure", err.message)
+    }
+})
 
 
 module.exports = router;
